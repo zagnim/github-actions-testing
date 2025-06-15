4f27764675af566f097044f82d7d1840aa870b8b
Nima Abdollahi <55274614+zagnim@users.noreply.github.com>
2025-06-15 13:42:04 +0200

    Creating test workflow

--- src/test/java/com/demo/project/ProjectApplicationTests.java
+++ src/test/java/com/demo/project/ProjectApplicationTests.java
@@ -3,11 +3,22 @@ package com.demo.project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 @SpringBootTest
 class ProjectApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void  sumOfTwoPlusTwoShouldEqualFour() {
+        int sum = 2 + 2;
+        assertEquals(4, sum);
+
+
     }
 
 }
